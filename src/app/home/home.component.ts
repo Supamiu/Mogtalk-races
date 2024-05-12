79dad0697c4e5744c831ff141c2e73bc729fa488
Flavien Normand <contact@flavien-normand.fr>
2024-05-12 22:55:12 +0200

    feat(home): basic home page for now

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {RaceService} from "../database/race.service";
+import {Timestamp, where} from "@angular/fire/firestore";
+import {map} from "rxjs";
+import {AsyncPipe} from "@angular/common";
+import {LeaderboardComponent} from "../leaderboard/leaderboard.component";
+import {NzFlexDirective} from "ng-zorro-antd/flex";
+import {NzDividerComponent} from "ng-zorro-antd/divider";
+import {NzEmptyComponent} from "ng-zorro-antd/empty";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    AsyncPipe,
+    LeaderboardComponent,
+    NzFlexDirective,
+    NzDividerComponent,
+    NzEmptyComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.less'
 })
 export class HomeComponent {
+  #raceService = inject(RaceService)
+
+  runningRace$ = this.#raceService.query(where('start', '<=', Timestamp.now())).pipe(
+    map(races => {
+      return races.filter(r => !r.stopped)[0]
+    })
+  )
 
 }
